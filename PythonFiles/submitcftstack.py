36edbb1ba1575771f0100a94745d86139ec238dd
Swastik <swastiksuman@gmail.com>
2021-09-26 23:44:52 +0530

    parameterized yaml

--- PythonFiles/submitcftstack.py
+++ PythonFiles/submitcftstack.py
@@ -1,8 +1,10 @@
 import boto3
+import sys
 
 cf = boto3.client('cloudformation')
 with open('../aws-s3-simple.yaml') as file:
     template = file.read()
 print(template)
-response = cf.create_stack(StackName='MyStack20210925', TemplateBody=template)
+print("Creating New Stack: "+sys.argv[1]+ " Bucket Name: "+ sys.argv[2])
+response = cf.create_stack(StackName=sys.argv[1], TemplateBody=template, Parameters=[{"ParameterKey":"BucketNameParam", "ParameterValue": sys.argv[2]}])
 print(response)
